a07ecb583fc7ec745053cac04d3c92183c3377fa
diego40g <diegopaznaula@hotmail.com>
2022-11-21 22:56:38 -0500

    install express and nodemon with get example

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+
+const port = 3000
+
+app.get('/home', (req, res) => {
+    return res.send('home screen')
+})
+
+app.listen(port, () => {
+    console.log(`Server listen on port ${port}`)
+})
