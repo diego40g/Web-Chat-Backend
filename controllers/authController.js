e3ecabad21338307cb00c2afb10bdeccf7170cc1
diego40g <diegopaznaula@hotmail.com>
2022-11-23 22:38:02 -0500

    add json web token to generate tokens of 1 week to login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models').User
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 exports.login = async(req, res) => {
     const { email, password } = req.body
@@ -21,15 +22,20 @@ exports.login = async(req, res) => {
             return res.status(401).json({ message: 'Incorrect password!'})
         }
         //generate auth token
-        
-        return res.send(user)
+        const userWithToken = generateToken(user.get({ raw: true }));
+        return res.send(userWithToken)
     } catch (e) {
-        
+        return res.status(500).json({ message: e.message })
     }
-
-    return res.send([email, password])
 }
 
 exports.register = async(req,res)=>{
 
+}
+
+const generateToken = (user) => {
+    delete user.password
+
+    const token = jwt.sign(user, 'secret', { expiresIn: 86400 })
+    return { ...user, ...{ token }} 
 }
