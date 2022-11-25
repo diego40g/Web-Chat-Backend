189d3ae0f02f06008e287004357f678769b1c352
diego40g <diegopaznaula@hotmail.com>
2022-11-24 23:28:32 -0500

    add express validator of parameters empty and email into the body params

--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
 const { login, register } = require('../controllers/authController')
+const { body } = require('express-validator')
 
 router.post('/login', login)
-router.post('/register', register)
+router.post('/register', [
+    body('firstName').notEmpty(),
+    body('lastName').notEmpty(),
+    body('gender').notEmpty(),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+],register)
 
 module.exports = router
