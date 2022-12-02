7787d612007d042ec5f4d6532fb21df2655c1017
diego40g <diegopaznaula@hotmail.com>
2022-12-01 20:31:30 -0500

    return user with object

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ exports.login = async(req, res) => {
         }
         //generate auth token
         const userWithToken = generateToken(user.get({ raw: true }));
-        userWithToken.avatar = user.avatar
+        userWithToken.user.avatar = user.avatar
         return res.send(userWithToken)
     } catch (e) {
         return res.status(500).json({ message: e.message })
@@ -45,5 +45,5 @@ const generateToken = (user) => {
     delete user.password
 
     const token = jwt.sign(user, config.appKey, { expiresIn: 86400 })
-    return { ...user, ...{ token }} 
+    return { ...{ user }, ...{ token }} 
 }
