a32ee67b617f406cd6eb02708bcd291ced8fb15e
diego40g <diegopaznaula@hotmail.com>
2022-12-03 23:59:52 -0500

    call validate functions

--- router/user.js
+++ router/user.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const { update } = require('../controllers/userController')
 const { validate } = require('../validators')
+const { rules: updateRules} = require('../validators/auth/user/update')
 const { auth } = require('../middleware/auth')
 
-router.post('/update', [auth], update)
+router.post('/update', [auth, updateRules, validate], update)
 
 module.exports = router
