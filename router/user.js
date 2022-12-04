c7e68cf4499f5e5308a68436ee53dd302be2770a
diego40g <diegopaznaula@hotmail.com>
2022-12-03 23:49:28 -0500

    create middleware with using jwt to validete tokens of users

--- router/user.js
+++ router/user.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const { update } = require('../controllers/userController')
 const { validate } = require('../validators')
+const { auth } = require('../middleware/auth')
 
-router.post('/update', [], update)
+router.post('/update', [auth], update)
 
 module.exports = router
