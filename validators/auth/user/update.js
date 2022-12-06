219106098a8dfd7ec35de9f8ccbb31c197e54b6e
diego40g <diegopaznaula@hotmail.com>
2022-12-05 21:45:44 -0500

    correct name in lastname to lastName

--- validators/auth/user/update.js
+++ validators/auth/user/update.js
@@ -3,7 +3,7 @@ const { body } =require('express-validator')
 exports.rules = (() => {
     return [
         body('firstName').notEmpty(),
-        body('lastname').notEmpty(),
+        body('lastName').notEmpty(),
         body('gender').notEmpty(),
         body('email').isEmail(),
         body('password').optional().isLength({ min: 5})
