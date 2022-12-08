b8c30f352fae35e1fc98caf1345451ff1d1b2afd
diego40g <diegopaznaula@hotmail.com>
2022-12-08 00:15:35 -0500

    the default message creating by sequelize command

---
+++ database/migrations/20221208051430-create-message.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      message: {
+        type: Sequelize.TEXT
+      },
+      chatId: {
+        type: Sequelize.INTEGER
+      },
+      fromUserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
