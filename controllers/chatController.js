3a1383f7fa7839bfb443cb93b1da0f1e76ff37fd
diego40g <diegopaznaula@hotmail.com>
2022-12-10 18:55:29 -0500

    create endpoint create chat with user id param by token user

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,6 +4,7 @@ const Chat = models.Chat
 const ChatUser = models.ChatUser
 const Message = models.Message
 const { Op } = require('sequelize')
+const { sequelize } = require('../models')
 
 exports.index = async (req, res) => {
     const user = await User.findOne({
@@ -33,4 +34,73 @@ exports.index = async (req, res) => {
     })
 
     return res.send(user.Chats)
+}
+
+exports.create = async (req, res) => {
+    const { partnerId } = req.body
+    const t = await sequelize.transaction()
+    try {
+        const user = await User.findOne({
+            where: {
+                id: req.user.id
+            },
+            include: [
+                {
+                    model: Chat,
+                    where: {
+                        type: 'dual'
+                    },
+                    include: [
+                        {
+                            model: ChatUser,
+                            where: {
+                                userId: partnerId
+                            }
+                        }
+                    ]
+                }
+            ]
+        })
+
+        if(user && user.Chats.length >0) 
+            return res.status(403).json({ status: 'Error', message: 'Chat with this user already exists!!!' })
+        const chat = await Chat.create({ type: 'dual' }, { transaction: t })
+        
+        await ChatUser.bulkCreate([
+            {
+                chatId: chat.id,
+                userId: req.user.id
+            },
+            {
+                chatId: chat.id,
+                userId: partnerId
+            }
+        ], { transaction: t })
+
+        await t.commit()
+
+        const newChat = await Chat.findOne({
+            where: {
+                id: chat.id
+            },
+            include: [
+                {
+                    model: User,
+                    where: {
+                        [Op.not]: {
+                            id: req.user.id
+                        }
+                    }
+                },
+                {
+                    model: Message
+                }
+            ]
+        })
+
+        return res.send(newChat)
+    } catch (error) {
+        await t.rollback()
+        return res.status(500).json({ status: 'Error', message: error.message })
+    }
 }
