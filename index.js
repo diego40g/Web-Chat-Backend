7481070abf184746b1e372705623da30e67652b8
diego40g <diegopaznaula@hotmail.com>
2022-12-16 22:31:56 -0500

    import socket index into server with http methods

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser= require('body-parser')
-
+const http = require('http')
 const config = require('./config/app');
 const router = require('./router');
-
+const SocketServer = require('./socket')
 const cors = require('cors')
 const app = express();
 
@@ -16,7 +16,9 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/uploads'))
 
 const port = config.appPort;
+const server = http.createServer(app)
+SocketServer(server)
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server listen on port ${port}`)
 })
