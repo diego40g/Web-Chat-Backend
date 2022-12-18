8168e518c65e4c010dd886209c6ad321c613c812
diego40g <diegopaznaula@hotmail.com>
2022-12-17 23:47:26 -0500

    map users to see is online of his friends and for friends

--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,49 @@
 const socketIo = require('socket.io')
 
+const users = new Map()
+
 const SocketServer = (server) => {
     const io = socketIo(server)
 
     io.on('connection', (socket) => {
         socket.on('join', async (user) => {
+            let sockets = []
+            if(users.has(user.id)) {
+                const existingUser = users.get(user.id)
+                existingUser.sockets = [...existingUser.sockets, ...[socket.id]]
+                users.set(user.id, existingUser)
+                sockets = [...existingUser.sockets, ...[socket.id]]
+            } else {
+                users.set(user.id, { id: user.id, sockets: [socket.id] })
+                sockets.push(socket.id)
+            }
+            const onlineFriends = [] //ids
+            const chatters = [] //query
+
+            // notify his friend that user is now online
+            for (let i=0; i<chatters.length; i++) {
+                if(users.has(chatters[i])) {
+                    const chatter = users.get(chatters[i])
+                    chatters.sockets.forEach(socket => {
+                        try {
+                            io.to(socket).emit('online', user)
+                        } catch (error) {
+                            
+                        }
+                    })
+                    onlineFriends.push(chatter.id)
+                }
+            }
+
+            //send to user sockets which of his friends are online
+            sockets.forEach(socket => {
+                try {
+                    io.to(socket).emit('friends', onlineFriends)
+                } catch (error) {
+                    
+                }
+            })
+
             console.log('New user joined: ', user.firstName);
 
             io.to(socket.id).emit('typing', 'User typing...')
