f95632c54c582a7ddf5cb39bce6f769cf34bd402
diego40g <diegopaznaula@hotmail.com>
2022-12-18 23:24:41 -0500

    adding a disconnect socket

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,7 @@
 const socketIo = require('socket.io')
 const { sequelize } = require('../models')
 const users = new Map()
+const userSockets = new Map()
 
 const SocketServer = (server) => {
     const io = socketIo(server)
@@ -13,9 +14,11 @@ const SocketServer = (server) => {
                 existingUser.sockets = [...existingUser.sockets, ...[socket.id]]
                 users.set(user.id, existingUser)
                 sockets = [...existingUser.sockets, ...[socket.id]]
+                userSockets.set(socket.id, user.id)
             } else {
                 users.set(user.id, { id: user.id, sockets: [socket.id] })
                 sockets.push(socket.id)
+                userSockets.set(socket.id, user.id)
             }
             const onlineFriends = [] //ids
             const chatters = await getChatters(user.id) //query
@@ -49,6 +52,35 @@ const SocketServer = (server) => {
             console.log('New user joined: ', user.firstName);
 
             io.to(socket.id).emit('typing', 'User typing...')
+        })
+
+        socket.on('disconnect', async() => {
+            if(userSockets.has(socket.id)) {
+                const user = users.get(userSockets.get(socket.id))
+                if(user.sockets.length > 1) {
+                    user.sockets = user.sockets.filter(sock => {
+                        if(sock !== socket.id) return true
+                        userSockets.delete(sock)
+                        return false
+                    })
+                    users.set(user.id, user)
+                }else{
+                    const chatters = getChatters(user.id)
+                    for (let i=0; i<chatters.length; i++){
+                        if (users.has(chatters[i])){
+                            users.get(chatters[i]).sockets.forEach(socket => {
+                                try{
+                                    io.to(socket).emit('offline', user)
+                                }catch(error){
+
+                                }
+                            })
+                        }
+                    }
+                    userSockets.delete(socket.id)
+                    users.delete(user.id)
+                }
+            }
         })
     })
 }
